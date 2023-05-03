c6a01b857c1e5b2e5adeb6ccafbc52dc7d818c5a
alexestebanupc <alex.esteban.rodriguez@estudiantat.upc.edu>
2023-05-03 19:16:12 +0200

    feat: only 1 theme

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ module.exports = function (eleventyConfig) {
   
   eleventyConfig.addPassthroughCopy('./src/css/'); // on build, it copies the files and adds them to output directory
   eleventyConfig.addWatchTarget('./src/css/'); // changes trigger a build
-  eleventyConfig.addPassthroughCopy('./src/theme.js');
 
   return {
     dir: {
