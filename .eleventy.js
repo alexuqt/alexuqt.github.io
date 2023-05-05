7a27b65510a5d71a683cded07a4cfd534338cfa2
alexestebanupc <alex.esteban.rodriguez@estudiantat.upc.edu>
2023-05-05 21:16:15 +0200

    fix: allow github pages using output

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
-  
+
   eleventyConfig.addPassthroughCopy('./src/css/'); // on build, it copies the files and adds them to output directory
   eleventyConfig.addWatchTarget('./src/css/'); // changes trigger a build
 
@@ -10,7 +10,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: 'src',
-      output: 'public',
+      output: 'docs',
     },
   };
 };
